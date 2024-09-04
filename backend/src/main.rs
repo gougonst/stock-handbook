f604e7b05d675adc0abf2d3f4bea78bb9441de8d
gougon <gougonst@gmail.com>
2024-09-05 01:10:26 +0800

    [NEW] Connect to mongoDB - example

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use mongodb::{ 
+	bson::{Document, doc},
+	Client,
+	Collection 
+};
+use std::env;
+
+mod configs;
+mod strings;
+
+#[tokio::main]
+async fn main() -> mongodb::error::Result<()> {
+    // Replace the placeholder with your Atlas connection string
+    let uri = match env::var(configs::MONGODB_CONN_STR_ENV) {
+        Ok(uri) => {
+            uri
+        }, 
+        _ => {
+            panic!("{}", strings::GET_MONGODB_CONN_STR_ENV_FAIL);
+        }
+    };
+    // Create a new client and connect to the server
+    let client = Client::with_uri_str(uri).await?;
+    // Get a handle on the movies collection
+    let database = client.database("sample_mflix");
+    let my_coll: Collection<Document> = database.collection("movies");
+    // Find a movie based on the title value
+    let my_movie = my_coll.find_one(doc! { "title": "The Perils of Pauline" }).await?;
+    // Print the document
+    println!("Found a movie:\n{:#?}", my_movie);
+    Ok(())
 }
---
+++ backend/src/strings.rs
@@ -0,0 +1,1 @@
+pub const GET_MONGODB_CONN_STR_ENV_FAIL: &str = "Get mongodb connection string from environment variable failed.";
