bf4dedfe232b4ce2bdaea4b6cb020b8ad738fa78
gougonst <gougonst@gmail.com>
2024-09-18 00:08:52 +0800

    [NEW] Implement login request

--- frontend/src/components/UserInfoComponent/UserInfoComponent.js
+++ frontend/src/components/UserInfoComponent/UserInfoComponent.js
@@ -1,3 +1,29 @@
 export default {
-    name: 'UserInfoComponent'
+    name: 'UserInfoComponent', 
+    props: {
+        username: String, 
+        password: String, 
+    }, 
+    emits: [
+        'update:username', 
+        'update:password'
+    ], 
+    computed: {
+        localUsername: {
+            get() {
+                return this.username;
+            }, 
+            set(val) {
+                this.$emit("update:username", val);
+            }
+        }, 
+        localPassword: {
+            get() {
+                return this.password;
+            }, 
+            set(val) {
+                this.$emit("update:password", val);
+            }
+        }
+    }
 }
--- frontend/src/components/UserLogin/UserLogin.js
+++ frontend/src/components/UserLogin/UserLogin.js
@@ -1,3 +1,33 @@
+import axios from "axios"
+import { API_BASE_URL, LOGIN_API, API_TIMEOUT } from "@/constants"
+
+const login_url = `${API_BASE_URL}${LOGIN_API}`;
+
 export default {
-    name: 'UserLogin'
+    name: 'UserLogin', 
+    data() {
+        return {
+            username: "", 
+            password: "", 
+        };
+    }, 
+    methods: {
+        async login() {
+            try {
+                console.log(login_url);
+                const resp = await axios.post(login_url, {
+                    username: this.username, 
+                    password: this.password
+                }, {
+                    timeout: API_TIMEOUT, 
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                });
+                console.log("Response: ", resp);
+            } catch (err) {
+                console.error("Login error: ", err);
+            }
+        }
+    }
 }
