65bdb2acc2d3a8cbf9074b1ea360c514a02da2a6
freddy_wen <freddy_wen@trendmicro.com>
2024-10-10 19:36:30 +0800

    [FIX] Change backend API to 0.0.0.0

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://192.168.1.148:8081";
+export const API_BASE_URL = "http://0.0.0.0:8081";
 export const LOGIN_API = "/auth/login";
 export const LOGON_API = "/auth/logon";
 export const API_TIMEOUT = 5000;
